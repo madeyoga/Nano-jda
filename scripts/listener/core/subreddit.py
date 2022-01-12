c9e3eb530fd8f926aaf05d458a424c484f447b3c
MadeYoga <vngla21@gmail.com>
2022-01-12 23:04:19 +0800

    Add subreddit.py

---
+++ scripts/listener/core/subreddit.py
@@ -0,0 +1,81 @@
+class Subreddits:
+    MEMES = "memes"
+    DANKMEMES = "dankmemes"
+    WTF = "wtf"
+    GRANDORDER = "grandorder"
+    WAIFU = "Waifu"
+    SCATHACH = "scathach"
+    FGOFANART = "FGOfanart"
+    ANIME = "anime"
+    ANIMEMES = "Animemes"
+    AWWNIME = "awwnime"
+    AZURELANE = "AzureLane"
+    TSUNDERES = "Tsunderes"
+    ANIMEWALLPAPER = "Animewallpaper"  # ANIME WALLPAPER ARTS
+    MOESCAPE = "Moescape"  # ANIME WALLPAPER ARTS
+    MAMARAIKOU = "MamaRaikou"
+    SABER = "Saber"
+    FGOCOMICS = "FGOcomics"
+    FATEPRISMAILLYA = "FatePrismaIllya"
+    ILLYASVIEL = "Illyasviel"
+
+
+subreddit_dictionary = {
+    "MEMES": "memes",
+    "DANKMEMES": "dankmemes",
+    "WTF": "wtf",
+    "GRANDORDER": "grandorder",
+    "WAIFU": "Waifu",
+    "SCATHACH": "scathach",
+    "FGOFANART": "FGOfanart",
+    "ANIME": "anime",
+    "ANIMEMES": "Animemes",
+    "AWWNIME": "awwnime",
+    "AZURELANE": "AzureLane",
+    "TSUNDERES": "Tsunderes",
+    "ANIMEWALLPAPER": "Animewallpaper",  # ANIME WALLPAPER ARTS
+    "MOESCAPE": "Moescape",  # ANIME WALLPAPER ARTS
+    "MAMARAIKOU": "MamaRaikou",
+    "SABER": "Saber",
+    "FGOCOMICS": "FGOcomics",
+    "FATEPRISMAILLYA": "FatePrismaIllya",
+    "ILLYASVIEL": "Illyasviel",
+    "HOLOLIVE": "Hololive",
+    "HOLOLIVEMEMES": "hololivememes",
+    "ONETRUETOHSAKA": "OneTrueTohsaka",
+    "CHURCHOFJEANNE": "ChurchOfJeanne"
+}
+
+fgo_subreddits = {
+    "MAMARAIKOU": "MamaRaikou",
+    "SABER": "Saber",
+    "FGOCOMICS": "FGOcomics",
+    "FATEPRISMAILLYA": "FatePrismaIllya",
+    "ILLYASVIEL": "Illyasviel",
+    "SCATHACH": "scathach",
+    "FGOFANART": "FGOfanart",
+    "GRANDORDER": "grandorder",
+    "ONETRUETOHSAKA": "OneTrueTohsaka",
+    "CHURCHOFJEANNE": "ChurchOfJeanne",
+    "MUSASHI_CHAN": "Musashi_chan"
+}
+
+other_subreddits = {
+    "MEMES": "memes",
+    "DANKMEMES": "dankmemes",
+    "WTF": "wtf"
+}
+
+anime_subreddits = {
+    "WAIFU": "Waifu",
+    "AWWNIME": "awwnime",
+    "ANIMEMES": "Animemes",
+    "TSUNDERES": "Tsunderes",
+    "AZURELANE": "AzureLane",
+    "ANIMEWALLPAPER": "Animewallpaper",  # ANIME WALLPAPER ARTS
+    "MOESCAPE": "Moescape",  # ANIME WALLPAPER ARTS
+    "HOLOLIVE": "Hololive",
+    "HOLOLIVEMEMES": "hololivememes",
+    "BIKINIMOE": "BikiniMoe",
+    "CHURCHOFBOOTY": "ChurchofBooty"
+}
